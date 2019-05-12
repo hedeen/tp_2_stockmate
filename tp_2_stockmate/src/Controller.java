4a9fafc605aaecd2c9618d38750faac549c286fd
nath7298 <nath7298@stthomas.edu>
2019-05-12 15:57:47 -0500

    Added in configuration (and runtime folder) to create the .jar file which we can now run from the command line (rather than in eclipse) if we want

--- tp_2_stockmate/src/Controller.java
+++ tp_2_stockmate/src/Controller.java
@@ -84,30 +84,6 @@ public class Controller {
 			// Error handling in case issue occur before we get to while loop
 			StdOut.println("Program error on initialization " + e.getMessage());
 		}
-
-//		// load and print incomes from latest filing
-//		FilingProcessor fp1 = new FilingProcessor("GOOG", new String[] { "income" });
-//		System.out.println("GOOG has " + fp1.getFilingCount() + " filings");
-//		fp1.bufferMostRecentFiling();
-//		String[] mrfd = fp1.getMostRecentFilingData("income");
-//		int mostRecentYear = Integer.parseInt(mrfd[0]);
-//		int mostRecentPeriod = Integer.parseInt(mrfd[1]);
-//
-//		System.out.println("Most recent filing: " + mostRecentYear + "-" + mostRecentPeriod);
-//		System.out.println("\t has data of : " + fp1.getTagData(mostRecentYear, mostRecentPeriod, "income"));
-//		System.out.println("\t also attained with: " + mrfd[2]);
-//		System.out.println("We can preview all data with this:\n" + fp1.getFilingPreview());
-//
-//		// load and print all data from all filings
-//		FilingProcessor fp2 = new FilingProcessor("AAPL", new String[] { "eps", "epsd" });
-//		fp2.bufferAllFilings();
-//		System.out.println(fp2.getFilingPreview());
-//
-//		// load data from latest filing and then return data for specific tag
-//		FilingProcessor fp3 = new FilingProcessor("MSFT", new String[] { "income" });
-//		fp3.bufferMostRecentFiling();
-//		System.out.println("The most recent income for microsoft is " + fp3.getMostRecentFilingData("income")[2]);
-
 	}
 	
 	// Constant defined loop question that is used multiple times (defined here as field for better readability)
